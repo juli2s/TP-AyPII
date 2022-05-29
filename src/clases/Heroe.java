3028146ffadb786e679e0ff509ed8a5b0e18be68
juli2s <dossantos38529@estudiantes.untref.edu.ar>
2022-05-28 23:29:54 -0300

    clase heroe agregada

---
+++ src/clases/Heroe.java
@@ -0,0 +1,16 @@
+package clases;
+
+public class Heroe extends Personaje {
+
+	public Heroe(Integer velocidad, Integer fuerza, Integer resistencia, Integer destreza) {
+		super(velocidad, fuerza, resistencia, destreza);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int compareTo(Personaje o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

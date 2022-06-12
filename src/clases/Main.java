3742254a334aa46f311f7e02251508cc7cba5877
juli2s <dossantos38529@estudiantes.untref.edu.ar>
2022-06-12 18:24:54 -0300

    Ahora menu es una clase aparte, constructor creado en Juego

--- src/clases/Main.java
+++ src/clases/Main.java
@@ -8,24 +8,11 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Juego j = new Juego();
-		
-		/*
-		 * System.out.println("Ingrese la opción elegida: "); BufferedReader input = new
-		 * BufferedReader(new InputStreamReader(System.in)); String inputString = null;
-		 * try { inputString = input.readLine(); } catch (IOException e) {
-		 * 
-		 * e.printStackTrace(); } Integer opcion = Integer.parseInt(inputString);
-		 */
-
 		try {
 			
-			j.menu();
+			Juego j = Juego.getInstance();
 		
 		} catch (NumberFormatException e) {
-			
-			e.printStackTrace();
-		} catch (IOException e) {
 			
 			e.printStackTrace();
 		}
